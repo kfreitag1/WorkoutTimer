0dc4e508bb2aac9c15f0bcd036b0491d3345e0d0
Kieran Freitag <kfreitag@kieran.ca>
2023-10-07 21:07:48 -0700

    Created command prompt/input to get data from user (replicates how the terminal works by default with Scanner)

--- src/main/ui/TerminalWorkoutTimerApp.java
+++ src/main/ui/TerminalWorkoutTimerApp.java
@@ -1,10 +1,13 @@
 package ui;
 
+import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
 import model.Routine;
 import model.Segment;
 
@@ -18,7 +21,9 @@ public class TerminalWorkoutTimerApp {
     private TerminalSize terminalSize;
     private String applicationState; // One of: "main_menu", "routine", "running"
     private Routine activeRoutine;
+
     private String commandPrompt;
+    private String commandInput;
 
     // Public methods
     // --------------------------------------------------------------------------------------------
@@ -82,8 +87,23 @@ public class TerminalWorkoutTimerApp {
     }
 
     // handles input for the main menu application state
-    private boolean handleUserInputMainMenu(KeyStroke stroke) {
-        return true; // stub
+    private boolean handleUserInputMainMenu(KeyStroke stroke) throws IOException {
+        if (!stroke.getKeyType().equals(KeyType.Character)) {
+            return true;
+        }
+
+        switch (stroke.getCharacter()) {
+            case 'n': // new
+                String routineName = getCommandWithRenderDisplay("Name of routine: ");
+                activeRoutine = new Routine(routineName);
+                applicationState = "routine";
+                break;
+            case 'l': // load
+                break;
+            case 'q': // quit
+                return false;
+        }
+        return true;
     }
 
     // handles input for the routine application state
@@ -97,24 +117,31 @@ public class TerminalWorkoutTimerApp {
         return true; // stub
     }
 
-    // modifies the commandPrompt argument, make sure its always back to null whenever done
+    // modifies the commandPrompt, commandInput argument,
+    // make sure prompt always back to null whenever done
     private String getCommandWithRenderDisplay(String prompt) throws IOException {
         // Set prompt to global environment
         this.commandPrompt = prompt;
+        this.commandInput = "";
 
         // exits when the enter key is pressed
         // string builder because intellij wanted me to
-        StringBuilder command = new StringBuilder();
         while (true) {
+            renderScreen();
+
             KeyStroke stroke = screen.readInput();
             switch (stroke.getKeyType()) {
                 case Character:
-                    command.append(stroke.getCharacter());
-                    renderScreen();
+                    commandInput += stroke.getCharacter();
+                    break;
+                case Backspace:
+                    if (!commandInput.isEmpty()) {
+                        commandInput = commandInput.substring(0, commandInput.length() - 1);
+                    }
                     break;
                 case Enter:
-                    this.commandPrompt = null;
-                    return command.toString();
+                    commandPrompt = null;
+                    return commandInput;
             }
         }
     }
@@ -124,7 +151,7 @@ public class TerminalWorkoutTimerApp {
 
     // renders the terminal graphics on the screen and manages screen size changes
     private void renderScreen() throws IOException {
-        screen.setCursorPosition(null);
+        screen.setCursorPosition(TerminalPosition.TOP_LEFT_CORNER);
         screen.clear();
 
         // Update screen size
@@ -143,35 +170,66 @@ public class TerminalWorkoutTimerApp {
                 renderRunning(screen);
                 break;
         }
-
-        // TODO: command input stuff here, after all of the other rendering
-
+        renderCommandPromptAndInput(screen);
         screen.refresh();
     }
 
+    // command prompt can have newline characters
+    private void renderCommandPromptAndInput(Screen screen) {
+        if (commandPrompt != null) {
+            TerminalPosition startPosition = screen.getCursorPosition();
+            TextGraphics commandDraw = screen.newTextGraphics();
+
+            // Get length of the last line in the command prompt, and total number of lines
+            List<String> splitCommandPrompt = List.of(commandPrompt.split("\n"));
+            int numLines = splitCommandPrompt.size();
+            int lengthLastLine = splitCommandPrompt.get(splitCommandPrompt.size() - 1).length();
+
+            // Output whole command prompt on multiple lines
+            for (int i = 0; i < numLines; i++) {
+                TerminalPosition linePosition = startPosition.withRelativeRow(i);
+                commandDraw.putString(linePosition, splitCommandPrompt.get(i));
+            }
+
+            // Output the user input, and set the cursor appropriately
+            TerminalPosition commandPosition = startPosition.withRelative(lengthLastLine, numLines - 1);
+            commandDraw.putString(commandPosition, commandInput);
+            screen.setCursorPosition(commandPosition.withRelativeColumn(commandInput.length()));
+        }
+    }
+
     // handles input for the main menu application state
+    // modifies cursor position to be after whatever this is
     private void renderMainMenu(Screen screen) {
         TextGraphics draw = screen.newTextGraphics();
         draw.putString(0, 0, "press command: (n)ew (l)oad (q)uit");
+
+        screen.setCursorPosition(new TerminalPosition(0, 1));
     }
 
     // requires that activeRoutine is not null
     // handles input for the routine application state
+    // modifies cursor position to be after whatever this is
     private void renderRoutine(Screen screen) {
         TextGraphics draw = screen.newTextGraphics();
         draw.putString(0, 0, "press command: (c)lose (s)ave (p)lay (r)estart (a)dd (d)elete (e)dit");
-        draw.putString(1, 0, "Title: " + activeRoutine.getName());
+        draw.putString(0, 1, "Title: " + activeRoutine.getName());
 
         List<Segment> segments = activeRoutine.getFlattenedSegments();
 
         for (int i = 0; i < segments.size(); i++) {
-            draw.putString(2 + i, 0, segments.get(i).getName());
+            draw.putString(0, 2 + i, segments.get(i).getName());
         }
+
+        screen.setCursorPosition(new TerminalPosition(0, 2 + segments.size()));
     }
 
     // handles input for the running application state
+    // modifies cursor position to be after whatever this is
     private void renderRunning(Screen screen) {
         TextGraphics draw = screen.newTextGraphics();
         draw.putString(0, 0, "press command: (p)ause");
+
+        screen.setCursorPosition(new TerminalPosition(0, 1));
     }
 }
