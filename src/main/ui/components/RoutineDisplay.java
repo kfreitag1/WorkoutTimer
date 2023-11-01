23df21e8a2b41caa63c8f64823f0106e106d30cf
Kieran Freitag <kfreitag@kieran.ca>
2023-10-31 22:47:22 -0700

    Make list of segment displays scrollable

--- src/main/ui/components/RoutineDisplay.java
+++ src/main/ui/components/RoutineDisplay.java
@@ -4,28 +4,34 @@ import model.Routine;
 import model.Segment;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
-public class RoutineDisplay extends JPanel {
+public class RoutineDisplay extends JScrollPane {
     private final Routine routine;
 
+    private final JPanel body;
+
     public RoutineDisplay(Routine routine) {
-        super();
+        super(new JPanel(), JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         this.routine = routine;
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        this.getVerticalScrollBar().setUnitIncrement(3);
+
+        body = (JPanel) getViewport().getView();
+        body.setLayout(new BoxLayout(body, BoxLayout.Y_AXIS));
+
         this.refresh();
     }
 
     public void refresh() {
         // TODO: May need to update the segments in a more smart way if this is really slow to remove
         //       and add them every update of the
-        removeAll();
+        body.removeAll();
         for (Segment segment : routine.getSegments()) {
             SegmentDisplay segmentDisplay = new SegmentDisplay(segment);
-            add(segmentDisplay);
+            body.add(segmentDisplay);
         }
+        repaint();
         revalidate();
     }
-
-
 }
